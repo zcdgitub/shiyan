c29598ba9883a86d00b6e901e5516b957b6af3c9
hetao <haohetao@gmail.com>
2019-01-03 16:34:13 +0800

    同步db配置

--- webroot/protected/config/db.php
+++ webroot/protected/config/db.php
@@ -7,15 +7,15 @@
  * To change this template use File | Settings | File Templates.
  */
 return array(
-	'database'=>'epmms_181225',
+	'database' => 'epmms_181225',
 	'host'=>'pgsql',
 	'port'=>5432,
 	'dump_cmd'=>'pg_dump',
 	'restore_cmd'=>'pg_restore',
-	'connectionString' => 'pgsql:host=pgsql;port=5432;dbname=epmms_181225',
+	'connectionString' => 'pgsql:host=pgsql;port=5432;dbname=epmms_181225;application_name=ep_web',
 	'emulatePrepare' => true,
-	'username' => 'postgres',
-	'password' => 'root',
+	'username' => 'epmms_181225',
+	'password' => 'yn9DR+i0lBQNrYMX70DSbVxj',
 	'charset' => 'utf8',
 	'tablePrefix'=>'epmms_',
 	'pdoClass' => 'NestedPDO',
