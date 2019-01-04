48991636846e757299f5ce932652c3e6a3e0318c
cuibin <951252220@qq.com>
2019-01-04 14:51:08 +0800

    cuibin

--- webroot/protected/models/ConfigJackpot.php
+++ webroot/protected/models/ConfigJackpot.php
@@ -39,8 +39,8 @@ class ConfigJackpot extends Model
 		return array(
 			array('config_jackpot_start_balance, config_jackpot_lucky_balance, config_jackpot_end_balance, config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio', 'filter','filter'=>array($this,'empty2null')),
 			array('config_jackpot_fund, config_jackpot_start_time, config_jackpot_end_time ,config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio', 'required'),
-			array('config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio', 'numerical', 'integerOnly'=>true),
-			array('config_jackpot_start_balance, config_jackpot_lucky_balance, config_jackpot_end_balance, config_jackpot_fund', 'ext.validators.Decimal','precision'=>16,'scale'=>4),
+//			array('config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio', 'numerical', 'integerOnly'=>true),
+			array('config_jackpot_start_balance, config_jackpot_lucky_balance, config_jackpot_end_balance, config_jackpot_fund ,config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio', 'ext.validators.Decimal','precision'=>16,'scale'=>4),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('config_jackpot_id, config_jackpot_start_balance, config_jackpot_lucky_balance, config_jackpot_end_balance, config_jackpot_fund, config_jackpot_start_order_ratio, config_jackpot_lucky_order_ratio, config_jackpot_end_order_ratio, config_jackpot_start_time, config_jackpot_end_time', 'safe', 'on'=>'search'),
