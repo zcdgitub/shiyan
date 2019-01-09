ef8a5e9483e7effeb3ce917a31134196209dc185
cuibin <951252220@qq.com>
2019-01-09 19:16:05 +0800

    cuibin

--- webroot/protected/models/AwardPeriodSum.php
+++ webroot/protected/models/AwardPeriodSum.php
@@ -148,7 +148,7 @@ class awardPeriodSum extends Model
                 $pageSize=$_GET['limit'];
             if($this->awardPeriodSumType->sum_type_id == 6){
 //                $criteria->compare('"awardPeriodSumType".sum_type_id',@$this->awardPeriodSumType->sum_type_id);
-                $criteria->compare('"awardPeriodSumType".sum_type_id',array(4,5,6));
+                $criteria->compare('"awardPeriodSumType".sum_type_id',array(4));
 //                var_dump($criteria);exit;
 
             }else{
