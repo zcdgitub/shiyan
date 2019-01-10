54221aff5ec3df29f443178b7750bee16dd99632
cuibin <951252220@qq.com>
2019-01-10 18:20:36 +0800

    cuibin

--- webroot/protected/controllers/AwardDaySumController.php
+++ webroot/protected/controllers/AwardDaySumController.php
@@ -102,10 +102,11 @@ class AwardDaySumController extends Controller
                 $info['data'][$val['award_day_date']]['sumMoney'] += $val['award_day_currency'];
                 $info['data'][$val['award_day_date']]['time'] = $val['award_day_date'];
             }
-            
+
             foreach ($info['data'][$val['award_day_date']]['data'][$key] as $key=>$val){
                 $info['data'][$val['award_day_date']]['data'][$key]['award_day_sum_type']=$data['sumtype'][$val['award_day_sum_type']]['sum_type_name'];
             }
+            
             echo CJSON::encode($info);
             webapp()->end();
         }
