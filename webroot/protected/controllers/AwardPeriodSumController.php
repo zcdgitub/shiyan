ea1dfad6a947dc4d70c0e918a73dec816d350ee9
cuibin <951252220@qq.com>
2019-01-10 17:33:15 +0800

    cuibin

--- webroot/protected/controllers/AwardPeriodSumController.php
+++ webroot/protected/controllers/AwardPeriodSumController.php
@@ -83,11 +83,11 @@ class AwardPeriodSumController extends Controller
                 //$model->awardPeriodSumType->sum_type_id=$sum->sum_type_id;
                 //$data['sumtype'][$key]['awardPeriodSum']=$model->search()->getArrayData();
             }
-//            unset($data['sumtype'][3]);
-//            unset($data['sumtype'][4]);
-//            $data['sumtype'][5]['sum_type_name'] = '奖池奖金';
+            unset($data['sumtype'][1]);
+            unset($data['sumtype'][2]);
+            $data['sumtype'][3]['sum_type_name'] = '奖池奖金';
 //            $model->awardPeriodSumType->sum_type_id=$curSumType;
-            
+
             $data['periodsum']=$model->search()->getArrayData();
          
             foreach($data['periodsum']['data'] as $key=>$sum)
