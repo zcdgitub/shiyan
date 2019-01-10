54e94e42adf331081e737ed5d02428a0166ca7ff
cuibin <951252220@qq.com>
2019-01-10 18:14:10 +0800

    cuibin

--- webroot/protected/controllers/AwardDaySumController.php
+++ webroot/protected/controllers/AwardDaySumController.php
@@ -92,6 +92,7 @@ class AwardDaySumController extends Controller
                 $dayModel->award_day_memberinfo_id=$sum['award_day_sum_memberinfo_id'];
                 $data['periodsum']['data'][$key]=$dayModel->search()->getArrayData();
             }
+            
             foreach ($data['periodsum']['data'][0] as $key=>$val){
 //                $val['award_day_sum_type'] = $data['sumtype'][$val['award_day_sum_type']]['sum_type_name'];
                 $info['data'][$val['award_day_date']]['data'][$key] = $val;
