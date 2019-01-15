47be89f91b29d573c001b932c4c0e377b865d24c
zcdgitub <15538110796@163.com>
2019-01-15 12:24:01 +0800

    zc

--- webroot/protected/controllers/MemberinfoController.php
+++ webroot/protected/controllers/MemberinfoController.php
@@ -1865,8 +1865,8 @@ public function actionUpdateName($id=null){
         }else{
             $parents = Membermap::model()->findByPk(user()->id);
         }
-         $dat['a']=Membermap::model()->count("membermap_order=1 and membermap_path like '$parents->membermap_path%'");
-         $dat['b']=Membermap::model()->count("membermap_order=2 and membermap_path like '$parents->membermap_path%'");
+         $dat['a']=Membermap::model()->count("membermap_order=1 and membermap_path like '$parents->membermap_path/%'");
+         $dat['b']=Membermap::model()->count("membermap_order=2 and membermap_path like '$parents->membermap_path/%'");
           if($dat){
                 if(webapp()->request->isAjaxRequest){
                 header('Content-Type: application/json');
