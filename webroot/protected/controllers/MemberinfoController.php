6eb76fa160f782a373516a830826a1c1e2d169cb
zcdgitub <15538110796@163.com>
2019-01-15 11:35:39 +0800

    zc

--- webroot/protected/controllers/MemberinfoController.php
+++ webroot/protected/controllers/MemberinfoController.php
@@ -1860,7 +1860,6 @@ public function actionUpdateName($id=null){
     }
     public function actionTeam(){
         if(isset($_POST['Memberinfo']['account'])){
-           
             $parent=Memberinfo::model()->find("memberinfo_account=:account" ,[':account'=>$_POST['Memberinfo']['account']]);
             $parents = Membermap::model()->find($parent->memberinfo_id);    
             $dat['a']=Membermap::model()->count("membermap_order=1 and membermap_path like '$parents->membermap_path%'");
